dcc5fcaba40aed75672a250baa5f4132712d8f6a
ravee-interview <78839526+ravee-interview@users.noreply.github.com>
2021-02-28 12:42:00 -0800

    Update longest_substring_with_atmost_k_chars.py

--- Week3/Sliding_Window/longest_substring_with_atmost_k_chars.py
+++ Week3/Sliding_Window/longest_substring_with_atmost_k_chars.py
@@ -13,19 +13,19 @@ class Solution(object):
         right = 0
         counter = defaultdict(int)
         max_sub_len = 0
-        curr_len = 0
+        curr_len = 0 #for calculating curr lenght of the valid substring found.
         
         while right < len(s): 
             right_char = s[right]
             counter[right_char] +=1
-            curr_len +=1
+            curr_len +=1 #increment with eevry increment in right pointer
             
             if len(counter) <= k and curr_len > max_sub_len:
                 max_sub_len = curr_len
                
             while left < right and len(counter) > k: #increment left pointer
                 left_char = s[left]
-                curr_len -=1
+                curr_len -=1 #subtract with every increment in left pointer
                 counter[left_char] -=1
                 if counter[left_char] == 0:
                     del(counter[left_char])
